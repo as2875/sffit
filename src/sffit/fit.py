470270aa8497d51c8da0214f6bf804403c25ea7d
Alexander Shtyrov <ashtyrov@mrc-lmb.cam.ac.uk>
2024-06-26 13:41:02 +0100

    Created util module for I/O functions

--- src/sffit/fit.py
+++ src/sffit/fit.py
@@ -1,7 +1,5 @@
 import argparse
-import os
 import time
-from collections import defaultdict
 from functools import partial
 
 import jax
@@ -9,10 +7,10 @@ import jax.numpy as jnp
 import jax.scipy.stats as stats
 import jax.experimental.sparse as sparse
 import gemmi
-import numpy as np
 
 from . import dencalc
 from . import sampler
+from . import util
 
 
 def loglik_fn(
@@ -122,14 +120,6 @@ def inference_loop(rng_key, kernel, batches, initial_state, grad_vmap, atycounts
     return states
 
 
-def write_map(data, template, path):
-    result_map = gemmi.Ccp4Map()
-    result_map.grid = gemmi.FloatGrid(np.array(data, dtype=np.float32))
-    result_map.grid.copy_metadata_from(template.grid)
-    result_map.update_ccp4_header()
-    result_map.write_ccp4_map(path)
-
-
 def parse_args():
     parser = argparse.ArgumentParser()
 
@@ -180,83 +170,16 @@ def main():
 
     # load observations
     print("loading data")
+
     ccp4 = gemmi.read_ccp4_map(args.map)
     mpdata = ccp4.grid.array
-    st = gemmi.read_structure(args.model)
-    st.expand_ncs(gemmi.HowToNameCopiedChain.Short)
     msk = gemmi.read_ccp4_map(args.mask)
     mskdata = msk.grid.array
 
-    # atom typing
+    st = gemmi.read_structure(args.model)
     st_aty = gemmi.read_structure(args.model)
-    st_aty.remove_alternative_conformations()
-    st_aty.expand_ncs(gemmi.HowToNameCopiedChain.Short)
-
-    serial_map = {}
-    for c1, c2 in zip(st[0], st_aty[0]):
-        for r1, r2 in zip(c1, c2):
-            for atref in r2:
-                atname = atref.name
-                for ateq in r1[atname]:
-                    serial_map[ateq.serial] = atref.serial
-
-    monlib_path = os.environ["CLIBD_MON"]
-    resnames = st_aty[0].get_all_residue_names()
-    monlib = gemmi.read_monomer_lib(monlib_path, resnames)
-    topo = gemmi.prepare_topology(
-        st_aty,
-        monlib,
-        h_change=gemmi.HydrogenChange.NoChange,
-    )
-    nbdict = defaultdict(list)
-
-    for cra in st_aty[0].all():
-        nbdict[cra.atom.serial].append(cra.atom.element.atomic_number)
-
-    for bond in topo.bonds:
-        elems = [atom.element.atomic_number for atom in bond.atoms]
-        serials = [atom.serial for atom in bond.atoms]
-
-        for i in [True, False]:
-            nbdict[serials[i]].append(elems[not i])
-
-    for k in nbdict.keys():
-        nbdict[k] = tuple(
-            [nbdict[k][0]] + sorted(nbdict[k][1:]),
-        )
-
-    # load model parameters into arrays
-    n_atoms = st[0].count_atom_sites()
-    coords = np.empty((n_atoms, 3))
-    it92 = np.empty((n_atoms, 10))
-    umat = np.empty((n_atoms, 3, 3))
-    occ = np.empty(n_atoms)
-    atyhash = np.empty(n_atoms, dtype=int)
-    atydesc = np.zeros((n_atoms, 10), dtype=int)
-    atnames = np.empty(n_atoms, dtype="<U20")
-
-    for ind, cra in enumerate(st[0].all()):
-        coords[ind] = [cra.atom.pos.x, cra.atom.pos.y, cra.atom.pos.z]
-        it92[ind] = np.concatenate([cra.atom.element.c4322.a, cra.atom.element.c4322.b])
-        occ[ind] = cra.atom.occ
-
-        if cra.atom.aniso.nonzero():
-            umat[ind] = 8 * np.pi**2 * np.array(cra.atom.aniso.as_mat33().tolist())
-        else:
-            umat[ind] = cra.atom.b_iso * np.identity(3)
-
-        envdesc = nbdict[serial_map[cra.atom.serial]]
-        atnames[ind] = str(cra)
-        atyhash[ind] = hash(envdesc)
-        atydesc[ind, : len(envdesc)] = envdesc
-
-    unq, unq_ind, aty, atycounts = np.unique(
-        atyhash,
-        return_index=True,
-        return_inverse=True,
-        return_counts=True,
-    )
-    naty = len(unq)
+    coords, it92, umat, occ, aty, atycounts, atnames, atydesc, unq_ind = util.from_gemmi(st, st_aty)
+    naty = len(atycounts)
 
     print(f"{naty} atom types identified")
 
@@ -291,14 +214,14 @@ def main():
             bounds,
             bsize,
         )
-        write_map(v_iam, ccp4, args.im)
+        util.write_map(v_iam, ccp4, args.im)
 
     inds1d = jnp.argwhere(mskdata == 1)
-    inds1dr = jax.random.choice(rng_key, inds1d, axis=0, shape=((args.nsamples) * 512,))
+    inds1dr = jax.random.choice(rng_key, inds1d, axis=0, shape=((args.nsamples) * 64,))
     pts1dr = inds1dr * spacing
 
-    indsbatched = inds1dr.reshape(-1, 512, 3)
-    ptsbatched = pts1dr.reshape(-1, 512, 3)
+    indsbatched = inds1dr.reshape(-1, 64, 3)
+    ptsbatched = pts1dr.reshape(-1, 64, 3)
     batched = jnp.stack([ptsbatched, indsbatched], axis=1)
 
     if not args.params:
@@ -398,7 +321,7 @@ def main():
             bsize,
         ).reshape(bsize, bsize, bsize)
 
-        write_map(v_approx, ccp4, args.om)
+        util.write_map(v_approx, ccp4, args.om)
 
 
 if __name__ == "__main__":
---
+++ src/sffit/util.py
@@ -0,0 +1,86 @@
+import os
+from collections import defaultdict
+
+import gemmi
+import numpy as np
+
+
+def write_map(data, template, path):
+    result_map = gemmi.Ccp4Map()
+    result_map.grid = gemmi.FloatGrid(np.array(data, dtype=np.float32))
+    result_map.grid.copy_metadata_from(template.grid)
+    result_map.update_ccp4_header()
+    result_map.write_ccp4_map(path)
+
+
+def from_gemmi(st, st_aty):
+    st.expand_ncs(gemmi.HowToNameCopiedChain.Short)
+    st_aty.remove_alternative_conformations()
+    st_aty.expand_ncs(gemmi.HowToNameCopiedChain.Short)
+
+    serial_map = {}
+    for c1, c2 in zip(st[0], st_aty[0]):
+        for r1, r2 in zip(c1, c2):
+            for atref in r2:
+                atname = atref.name
+                for ateq in r1[atname]:
+                    serial_map[ateq.serial] = atref.serial
+
+    monlib_path = os.environ["CLIBD_MON"]
+    resnames = st_aty[0].get_all_residue_names()
+    monlib = gemmi.read_monomer_lib(monlib_path, resnames)
+    topo = gemmi.prepare_topology(
+        st_aty,
+        monlib,
+        h_change=gemmi.HydrogenChange.NoChange,
+    )
+    nbdict = defaultdict(list)
+
+    for cra in st_aty[0].all():
+        nbdict[cra.atom.serial].append(cra.atom.element.atomic_number)
+
+    for bond in topo.bonds:
+        elems = [atom.element.atomic_number for atom in bond.atoms]
+        serials = [atom.serial for atom in bond.atoms]
+
+        for i in [True, False]:
+            nbdict[serials[i]].append(elems[not i])
+
+    for k in nbdict.keys():
+        nbdict[k] = tuple(
+            [nbdict[k][0]] + sorted(nbdict[k][1:]),
+        )
+
+    # load model parameters into arrays
+    n_atoms = st[0].count_atom_sites()
+    coords = np.empty((n_atoms, 3))
+    it92 = np.empty((n_atoms, 10))
+    umat = np.empty((n_atoms, 3, 3))
+    occ = np.empty(n_atoms)
+    atyhash = np.empty(n_atoms, dtype=int)
+    atydesc = np.zeros((n_atoms, 10), dtype=int)
+    atnames = np.empty(n_atoms, dtype="<U20")
+
+    for ind, cra in enumerate(st[0].all()):
+        coords[ind] = [cra.atom.pos.x, cra.atom.pos.y, cra.atom.pos.z]
+        it92[ind] = np.concatenate([cra.atom.element.c4322.a, cra.atom.element.c4322.b])
+        occ[ind] = cra.atom.occ
+
+        if cra.atom.aniso.nonzero():
+            umat[ind] = 8 * np.pi**2 * np.array(cra.atom.aniso.as_mat33().tolist())
+        else:
+            umat[ind] = cra.atom.b_iso * np.identity(3)
+
+        envdesc = nbdict[serial_map[cra.atom.serial]]
+        atnames[ind] = str(cra)
+        atyhash[ind] = hash(envdesc)
+        atydesc[ind, : len(envdesc)] = envdesc
+
+    _, unq_ind, aty, atycounts = np.unique(
+        atyhash,
+        return_index=True,
+        return_inverse=True,
+        return_counts=True,
+    )
+
+    return coords, it92, umat, occ, aty, atycounts, atnames, atydesc, unq_ind
