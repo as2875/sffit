Bugfix: fixed errors in SF calculation

src/sffit/dencalc.py
@@ -17,10 +17,13 @@ def _add_and_cho(umat, b, sigma):
 @jax.jit
 def one_coef_3d(a, b, umat, sigma, pts):
     ucho = _add_and_cho(umat, b, sigma)
-    y = ucho @ pts.T
+    y = ucho.T @ pts.T
     s_U_s = jnp.linalg.vector_norm(y, axis=0) ** 2
 
     return a * jnp.exp(-s_U_s / 4)
+
+
+oc3d_vmap = jax.vmap(one_coef_3d, in_axes=[0, 0, None, None, None])
 
 
 @jax.jit
@@ -28,7 +31,7 @@ def _calc_f_atom(coord, umat, occ, it92, aty, weights, sigma, pts):
     f_at = (
         occ
         * weights[aty]
-        * ocre_vmap(
+        * oc3d_vmap(
             it92[:5],
             it92[5:],
             umat,
@@ -47,13 +50,13 @@ calc_f = jax.vmap(_calc_f_atom, in_axes=[0, 0, 0, 0, 0, None, None, None])
 @jax.jit
 def one_coef_re(a, b, umat, sigma, pts):
     ucho = _add_and_cho(umat, b, sigma)
-    y = triangular_solve(ucho, pts.T, left_side=True)
+    y = triangular_solve(ucho.T, pts.T, left_side=True)
     r_U_r = jnp.linalg.vector_norm(y, axis=0) ** 2
 
     udet = ucho[0, 0] * ucho[1, 1] * ucho[2, 2]
 
     den = (
-        a * (4 * jnp.pi) * jnp.sqrt(4 * jnp.pi) * jnp.exp(-4 * jnp.pi**2 * r_U_r)
+        a * 8 * jnp.pi * jnp.sqrt(jnp.pi) * jnp.exp(-4 * jnp.pi**2 * r_U_r)
     ) / udet
 
     return den
@@ -126,7 +129,7 @@ def make_grid(bounds, nsamples):
 def make_bins(data, spacing, dmax, nbins):
     axis = jnp.fft.fftfreq(data.shape[0], d=spacing)
 
-    sx, sy, sz = jnp.meshgrid(axis, axis, axis)
+    sx, sy, sz = jnp.meshgrid(axis, axis, axis, indexing="ij")
     s = jnp.sqrt(sx**2 + sy**2 + sz**2)
 
     bins = jnp.linspace(0, dmax, nbins + 1)

src/sffit/fit.py
@@ -26,6 +26,7 @@ def loglik_fn(
     atycounts,
     D,
     sigma_n,
+    data_size,
 ):
     weights, sigma = (
         params["weights"],
@@ -39,10 +40,9 @@ def loglik_fn(
     D_s = D[inds[:, 0], inds[:, 1], inds[:, 2]]
     sg_n_s = sigma_n[inds[:, 0], inds[:, 1], inds[:, 2]]
 
-    N, n = target.size, pts.shape[0]
     logpdf = -(
         jnp.log(jnp.pi) + jnp.log(sg_n_s) + jnp.abs(f_o - D_s * f_c) ** 2 / sg_n_s
-    ) * (N / n)
+    ) * (data_size / len(pts))
     logpdf = jnp.where(
         sg_n_s < 1e-6,
         jnp.nan,
@@ -177,7 +177,6 @@ def main():
     mpdata, coords, it92, umat, occ, aty, atycounts = [
         jnp.array(a) for a in (mpdata, coords, it92, umat, occ, aty, atycounts)
     ]
-    f_obs = jnp.fft.fftn(mpdata * mskdata)
 
     assert (
         ccp4.grid.nu == ccp4.grid.nv == ccp4.grid.nw
@@ -212,6 +211,8 @@ def main():
             "weights": jnp.ones(naty),
             "sigma": jnp.zeros(naty),
         }
+        fft_scale = ccp4.grid.unit_cell.volume / ccp4.grid.point_count
+        f_obs = jnp.fft.fftn(mpdata * mskdata)
 
         # initialise frequency grid
         freqs, fbins, bin_edges = dencalc.make_bins(
@@ -251,6 +252,7 @@ def main():
             atycounts=atycounts,
             D=D_gr,
             sigma_n=sg_n_gr,
+            data_size=len(inds1d),
         )
         logprior = partial(
             logprior_fn,